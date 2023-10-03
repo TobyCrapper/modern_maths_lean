ec80a7d1069e65b330f7225635e04788e9123d03
TobyCrapper <146371089+TobyCrapper@users.noreply.github.com>
2023-10-03 21:48:31 +0000

    2.1

--- src/expressions/types.lean
+++ src/expressions/types.lean
@@ -16,15 +16,26 @@ import data.rat
 
 Use the space below to evaluate the expressions `(1-4)^5`, `(1 + 4)`, `5/4+1/2`, and
 `66/2` first without Lean then with the Lean ``#eval`` command.
-
 -/
+
+-- (1-4)^5 = (-3)^5 = (-243) --
+#eval ((1-4)^5 : ℤ)
+
+-- (1+4) = (5) --
+#eval 1 + 4
+
+-- (5/4+1/2) = (1.25+0.5) = (1.75) --
+#eval (5/4+1/2 : ℚ)
+
+-- (66/2) = (33) --
+#eval 66/2
 
 
 /- Checking the *type* of the expression `3 = 1 + 2`.
-
 -/
 
 #check 3 = 1 + 2
+
 
 /- PROBLEM:
 
@@ -36,4 +47,9 @@ Use the space below to evaluate the expressions `(1-4)^5`, `(1 + 4)`, `5/4+1/2`,
   (i) `2 + 3`, (ii) `2 + 3 = 10`, (iii) 'for all integers `x`, `2x = 10`'.
 -/
 
+#eval (2 + 3 : ℕ)
+#check 5
 
+#check (2 + 3 = 10)
+
+#check (∀ (x : ℤ), 2 * x = 10)
